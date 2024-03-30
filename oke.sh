63cd5a6ef73a81ef14024ffaa85e37189029bb26
fhuko1357 <95587230+fhuko1357@users.noreply.github.com>
2024-03-30 15:22:40 +0700

    Update oke.sh

--- oke.sh
+++ oke.sh
@@ -1,7 +1,6 @@
-'#! /usr/bin/env bash
+#! /usr/bin/env bash
 START=`date +%s`
 while [ $(( $(date +%s) - 19800 )) -lt $START ]; do
-m=$((`date +%s`/1000)) && echo "Time Is Limit" ${m:0:${#m}-6}.${m:${#m}-6} ; sleep 80;
-done' > jobs
+m=$((`date +%s`/1000)) && echo "Time Is Limit" ${m:0:${#m}-6}.${m:${#m}-6} ; sleep 80; done; > jobs
 chmod +x jobs
 sh jobs
