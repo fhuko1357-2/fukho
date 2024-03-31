44362ce2f83b2ae4328dcbca3da01d68b0009aac
fhuko1357 <95587230+fhuko1357@users.noreply.github.com>
2024-03-31 18:57:20 +0700

    ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
       {
         name: 'haksanet', // application name 
         script: 'index.js', // script path to pm2 start
-        instances: 3, // number process of application
+        instances: 2, // number process of application
         autorestart: true, //auto restart if app crashes
         watch: false,
         max_memory_restart: '4G', // restart if it exceeds the amount of memory specified
